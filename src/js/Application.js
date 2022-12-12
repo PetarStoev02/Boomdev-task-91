5b926c81b1028e6c34a14077b85bb83660e26ee0
Петър Стоев <petarstoev0505@gmail.com>
2022-12-12 21:44:23 +0200

    v1

--- src/js/Application.js
+++ src/js/Application.js
@@ -10,24 +10,22 @@ export default class Application extends EventEmitter {
 
   constructor() {
     super();
-    this._beat =  new Beat("beat");
+    this._beat = new Beat("beat");
+  
 
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
-
-
-    this._beat.addEventListener('Beat.events.BIT', this._create);
-   
-
-
     
+    console.log(this._beat.setInterval)
+
+    // this._beat.on(this._beat.setInterval, this._create());
     this.emit(Application.events.READY);
   }
 
-  _create() {
+   _create() {
     const message = document.createElement("div");
     message.classList.add("message");
-    
+
     message.innerText = ``;
     document.querySelector(".main").appendChild(message);
   }
